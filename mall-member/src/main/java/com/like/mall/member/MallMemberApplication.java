91bab062144be6a80134aba497abeeb858374d68
like <980650920@qq.com>
2020-10-25 15:29:07 +0800

    添加nacos服务发现注解
    Signed-off-by: like <980650920@qq.com>

--- mall-member/src/main/java/com/like/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/like/mall/member/MallMemberApplication.java
@@ -2,8 +2,12 @@ package com.like.mall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.like.mall.member.feign") // 开启远程调用功能
 public class MallMemberApplication {
 
     public static void main(String[] args) {
--- mall-product/src/main/java/com/like/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/like/mall/product/MallProductApplication.java
@@ -3,8 +3,10 @@ package com.like.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.like.mall.product.dao")
 public class MallProductApplication {
 
