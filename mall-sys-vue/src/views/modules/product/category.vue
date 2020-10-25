9005ba5cc677682ff6b9c2580b520874995f1e81
like <980650920@qq.com>
2020-10-25 20:24:21 +0800

    添加配置文件
    Signed-off-by: like <980650920@qq.com>

---
+++ mall-sys-vue/src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [
+        
+      ],
+      defaultProps: {
+        children: 'children',
+        label: 'label',
+      },
+    }
+  },
+  methods: {
+     getmenu(){
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'GET',
+      }).then(res =>console.log(res.data))
+    },
+    handleNodeClick(data) {
+      console.log(data)
+    },
+  },
+  created() {
+    this.getmenu();
+  }
+}
+</script>
+<style>
+</style>
