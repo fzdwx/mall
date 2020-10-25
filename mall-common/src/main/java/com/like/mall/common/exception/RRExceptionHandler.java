c75dde86c68521edd40cd3720e2ae14f91b6978f
like <980650920@qq.com>
2020-10-25 13:24:29 +0800

    mall-init
    Signed-off-by: like <980650920@qq.com>

--- mall-common/src/main/java/com/like/mall/common/exception/RRExceptionHandler.java
+++ mall-common/src/main/java/com/like/mall/common/exception/RRExceptionHandler.java
@@ -9,7 +9,6 @@
 package com.like.mall.common.exception;
 
 import com.like.mall.common.utils.R;
-import org.apache.shiro.authz.AuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
@@ -49,12 +48,12 @@ public class RRExceptionHandler {
 		logger.error(e.getMessage(), e);
 		return R.error("数据库中已存在该记录");
 	}
-
-	@ExceptionHandler(AuthorizationException.class)
-	public R handleAuthorizationException(AuthorizationException e){
-		logger.error(e.getMessage(), e);
-		return R.error("没有权限，请联系管理员授权");
-	}
+//
+//	@ExceptionHandler(AuthorizationException.class)
+//	public R handleAuthorizationException(AuthorizationException e){
+//		logger.error(e.getMessage(), e);
+//		return R.error("没有权限，请联系管理员授权");
+//	}
 
 	@ExceptionHandler(Exception.class)
 	public R handleException(Exception e){
--- mall-product/src/test/java/com/like/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/like/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.like.mall.product;
 
+import com.like.mall.product.entity.BrandEntity;
+import com.like.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity ent = new BrandEntity();
+        ent.setDescript("hello");
+        ent.setName("like");
+        boolean save = brandService.save(ent);
+        System.out.println(save);
     }
 
+    @Test
+    void hello() {
+        System.out.println("hello");
+    }
 }
