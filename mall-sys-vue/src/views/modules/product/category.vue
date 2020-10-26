0164f3682c0be3bc5a7df02609cc20bf135247c5
like <980650920@qq.com>
2020-10-26 13:16:33 +0800

    sys-vue: 前端页面-商品系统-分页维护：页面搭建，添加添加和删除按钮逻辑（如果是一级标签就不显示删除只显示添加，如果还有子标签就不显示删除只显示添加）
    Signed-off-by: like <980650920@qq.com>

--- mall-sys-vue/src/views/modules/product/category.vue
+++ mall-sys-vue/src/views/modules/product/category.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false" 
+      show-checkbox="true"
+      node-key="catId"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <!-- 显示标题 -->
+        <span>{{ node.label }}</span>
+        <!-- 删除和添加按钮 -->
+        <span>
+          <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">添加</el-button>
+          <el-button
+            v-if="node.childNodes.length==0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >删除</el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 
@@ -8,29 +29,35 @@
 export default {
   data() {
     return {
-      data: [
-        
-      ],
+      menus: [],
       defaultProps: {
         children: 'children',
-        label: 'label',
+        label: 'name',
       },
     }
   },
   methods: {
-     getmenu(){
+    // 添加一个节点
+    append(data) {
+     
+    },
+    // 删除一个节点
+    remove(node, data) {
+    
+    },
+    // 获取分类信息
+    getmenu() {
+      // 发送请求
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'GET',
-      }).then(res =>console.log(res.data))
-    },
-    handleNodeClick(data) {
-      console.log(data)
+        // 赋值
+      }).then((res) => (this.menus = res.data.data))
     },
   },
   created() {
-    this.getmenu();
-  }
+    this.getmenu()
+  },
 }
 </script>
 <style>
