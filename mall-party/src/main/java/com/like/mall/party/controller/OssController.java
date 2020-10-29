5f83fb6e1d13da03892b95503f1f04b78c778dde
like <980650920@qq.com>
2020-10-29 21:07:59 +0800

    party:添加oss服务器签名请求
    Signed-off-by: like <980650920@qq.com>

---
+++ mall-party/src/main/java/com/like/mall/party/controller/OssController.java
@@ -0,0 +1,83 @@
+package com.like.mall.party.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author like
+ * @since 2020-10-29 14:54
+ */
+@RestController
+public class OssController {
+
+    @Autowired
+    OSS ossClient;
+
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    String endpoint;
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    String bucket;
+    @Value("${spring.cloud.alicloud.access-key}")
+    String accessId;
+    @Value("${spring.cloud.alicloud.secret-key}")
+    String accessKey;
+
+    /**
+     * 服务器签名
+     *
+     * @return 签名信息
+     * @throws Exception 异常
+     */
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy() throws Exception {
+        //https://mall-like.oss-cn-shenzhen.aliyuncs.com/QQ%E6%88%AA%E5%9B%BE20201014092430.png
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+        String callbackUrl = "http://88.88.88.88:8888";
+        String nowDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = nowDate + "/"; // 用户上传文件时指定的前缀。
+        Map<String, String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000)); // 过期时间
+            // respMap.put("expire", formatISO8601Date(expiration));
+
+
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+        return respMap;
+    }
+}
