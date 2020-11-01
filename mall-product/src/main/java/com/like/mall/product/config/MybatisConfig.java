fb3f66a60d85ffd422e4ace81cb599920479c529
like <980650920@qq.com>
2020-11-01 13:18:39 +0800

    product：添加分页插件
    Signed-off-by: like <980650920@qq.com>

---
+++ mall-product/src/main/java/com/like/mall/product/config/MybatisConfig.java
@@ -0,0 +1,28 @@
+package com.like.mall.product.config;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author like
+ * @since 2020-11-01 13:13
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.like.mall.product.dao")
+public class MybatisConfig {
+
+    // 分页插件
+    @Bean
+    public PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        // 设置请求的页面大于最后页面的操作，true 返回首页
+        paginationInnerInterceptor.setOverflow(true);
+        // 设置最大分页
+        paginationInnerInterceptor.setMaxLimit(500L);
+        return paginationInnerInterceptor;
+    }
+}
