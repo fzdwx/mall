7d0b2e798cbd5f4595e7f52a65fe9b92c59abc53
like <980650920@qq.com>
2020-11-01 13:27:58 +0800

    product：商品系统-品牌管理-添加模糊查询
    Signed-off-by: like <980650920@qq.com>

--- mall-product/src/main/java/com/like/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/like/mall/product/service/impl/BrandServiceImpl.java
@@ -1,16 +1,17 @@
 package com.like.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.like.mall.common.utils.PageUtils;
 import com.like.mall.common.utils.Query;
-
 import com.like.mall.product.dao.BrandDao;
 import com.like.mall.product.entity.BrandEntity;
 import com.like.mall.product.service.BrandService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -18,10 +19,19 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        // 1.组装查询条件
+        QueryWrapper<BrandEntity> query = new QueryWrapper<>();
+        // - 获取key
+        String key = params
+                .get("key")
+                .toString();
+        if (StringUtils.isNotBlank(key)) {
+            query.eq("brand_id", key).or().like("name", key);
+        }
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
-        );
+                query
+                                           );
 
         return new PageUtils(page);
     }
