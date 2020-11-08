ad26bd4514d8f14285fce874910817d092dfcfd8
like <980650920@qq.com>
2020-11-08 17:40:14 +0800

    采购单维护-搜索
    Signed-off-by: like <980650920@qq.com>

--- mall-ware/src/main/java/com/like/mall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ mall-ware/src/main/java/com/like/mall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,16 +1,17 @@
 package com.like.mall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.like.mall.common.utils.PageUtils;
 import com.like.mall.common.utils.Query;
-
 import com.like.mall.ware.dao.PurchaseDetailDao;
 import com.like.mall.ware.entity.PurchaseDetailEntity;
 import com.like.mall.ware.service.PurchaseDetailService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("purchaseDetailService")
@@ -18,9 +19,26 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> query = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotBlank(key)) {
+            query.and(q -> {
+                q.eq("purchase_id", key).or().eq("sku_id", key);
+            });
+        }
+        String status = (String) params.get("status");
+        if (StringUtils.isNotBlank(status)) {
+            query.eq("status", status);
+        }
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotBlank(wareId)) {
+            query.eq("ware_id", wareId);
+        }
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                query
         );
 
         return new PageUtils(page);
