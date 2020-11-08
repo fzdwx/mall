b3e94e86ca94e70e28ad3f49cca532e9e03423a6
like <980650920@qq.com>
2020-11-08 17:33:30 +0800

    商品库存-搜索
    Signed-off-by: like <980650920@qq.com>

--- mall-ware/src/main/java/com/like/mall/ware/service/impl/WareSkuServiceImpl.java
+++ mall-ware/src/main/java/com/like/mall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,16 +1,17 @@
 package com.like.mall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.like.mall.common.utils.PageUtils;
 import com.like.mall.common.utils.Query;
-
 import com.like.mall.ware.dao.WareSkuDao;
 import com.like.mall.ware.entity.WareSkuEntity;
 import com.like.mall.ware.service.WareSkuService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareSkuService")
@@ -18,9 +19,20 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> query = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if (StringUtils.isNotBlank(skuId)) {
+            query.eq("sku_id",skuId);
+
+        }
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotBlank(wareId)) {
+            query.eq("ware_id",wareId);
+
+        }
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                query
         );
 
         return new PageUtils(page);
