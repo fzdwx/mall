e475c0c1ac61ae9e8cd7c0c364568dfdcdc6c231
like <980650920@qq.com>
2020-11-08 17:28:05 +0800

    商品管理：sku搜索功能
    Signed-off-by: like <980650920@qq.com>

--- mall-ware/src/main/java/com/like/mall/ware/service/impl/WareInfoServiceImpl.java
+++ mall-ware/src/main/java/com/like/mall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,29 +1,38 @@
 package com.like.mall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.like.mall.common.utils.PageUtils;
 import com.like.mall.common.utils.Query;
-
 import com.like.mall.ware.dao.WareInfoDao;
 import com.like.mall.ware.entity.WareInfoEntity;
 import com.like.mall.ware.service.WareInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
 public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity> implements WareInfoService {
 
-    @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        IPage<WareInfoEntity> page = this.page(
-                new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
-        );
+        @Override
+        public PageUtils queryPage(Map<String, Object> params) {
+            QueryWrapper<WareInfoEntity> query = new QueryWrapper<>();
+            String key = (String) params.get("key");
+            if (StringUtils.isNotBlank(key)) {
+                query.eq("id",key)
+                        .or().like("name",key)
+                        .or().like("address",key)
+                        .or().like("areacode",key);
+            }
+            IPage<WareInfoEntity> page = this.page(
+                    new Query<WareInfoEntity>().getPage(params),
+                    query
+            );
 
-        return new PageUtils(page);
-    }
+            return new PageUtils(page);
+        }
 
 }
