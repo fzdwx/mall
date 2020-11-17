6f93dd7092ff32ced1e08f40a82c7c3b1cd0c21d
like <980650920@qq.com>
2020-11-17 08:56:58 +0800

    添加es：RequestOptions配置
    Signed-off-by: like <980650920@qq.com>

--- mall-search/src/main/java/com/like/mall/search/config/ESConfig.java
+++ mall-search/src/main/java/com/like/mall/search/config/ESConfig.java
@@ -1,6 +1,7 @@
 package com.like.mall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +15,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ESConfig {
 
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+       /* builder.addHeader("Authorization","Bearer"+TOKEN)
+                .setHttpAsyncResponseConsumerFactory(new HttpAsyncResponseConsumerFactory
+                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));*/
+        COMMON_OPTIONS = builder.build();
+    }
+
+    /**
+     * 注册es客户端
+     *
+     * @return {@link RestHighLevelClient}
+     */
     @Bean
     public RestHighLevelClient esClient() {
         return new RestHighLevelClient(
-                RestClient.builder(new HttpHost("47.112.150.204",9200,"http"))
+                RestClient.builder(new HttpHost("47.112.150.204", 9200, "http"))
         );
     }
 
