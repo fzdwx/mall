719cbf7f912a896c44c13e7d2f85bb3f7a66b6d5
like <980650920@qq.com>
2020-12-03 19:13:26 +0800

    测试
    Signed-off-by: like <980650920@qq.com>

--- mall-product/src/main/java/com/like/mall/product/controller/web/IndexController.java
+++ mall-product/src/main/java/com/like/mall/product/controller/web/IndexController.java
@@ -3,6 +3,9 @@ package com.like.mall.product.controller.web;
 import com.like.mall.product.entity.CategoryEntity;
 import com.like.mall.product.service.CategoryService;
 import com.like.mall.product.vo.Catelog2Vo;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author like
@@ -40,4 +44,37 @@ public class IndexController {
     public Map<String, List<Catelog2Vo>> getCatalogJson() {
         return categoryService.getCatalogJson();
     }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        // 1.获取锁
+        RLock lock = redisson.getLock("hello");
+
+        // 2.上锁
+        /**
+         * a、锁会自动续期，默认续期到30s，不用担心业务时间过长，导致锁过期被删除
+         * b、加锁的业务只要运行完成，就不会续期，当完成后就会在30s内删除
+         */
+        lock.lock();
+        try {
+            System.out.println("加锁成功,执行业务"+Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("解锁"+Thread.currentThread().getName());
+            // 解锁
+            lock.unlock();
+        }
+
+        return "hello";
+    }
+
+    @Autowired
+    RedissonClient redisson;
 }
