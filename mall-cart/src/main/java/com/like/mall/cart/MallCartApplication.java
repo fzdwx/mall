606acd3acbb92ad93b82ee7b1d0f2cda3c4e3f51
like <980650920@qq.com>
2020-12-23 19:19:30 +0800

    cart 配置文件修改
    Signed-off-by: like <980650920@qq.com>

--- mall-cart/src/main/java/com/like/mall/cart/MallCartApplication.java
+++ mall-cart/src/main/java/com/like/mall/cart/MallCartApplication.java
@@ -2,8 +2,15 @@ package com.like.mall.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableRedisHttpSession
 public class MallCartApplication {
 
     public static void main(String[] args) {
