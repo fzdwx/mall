21f0aaf7e3501b5e3a98b258cacc20f7256a557f
like <980650920@qq.com>
2020-12-30 15:59:53 +0800

    RabbitMQ设置发送端确认2
    Signed-off-by: like <980650920@qq.com>

--- mall-order/src/main/java/com/like/mall/order/config/RabbitMQConfig.java
+++ mall-order/src/main/java/com/like/mall/order/config/RabbitMQConfig.java
@@ -1,5 +1,6 @@
 package com.like.mall.order.config;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -29,11 +30,28 @@ public class RabbitMQConfig {
 
     @PostConstruct
     public void initRabbitTemplate() {
-        // 设置确认回调-确认抵达
+        // 设置确认回调
         rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
             @Override
-            public void confirm(CorrelationData correlationData, boolean b, String s) {
-                System.out.println("confirm{correlationData:"+correlationData+",ack:"+b+",cause:"+s);
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm{correlationData:"+correlationData+",ack:"+ack+",cause:"+cause);
+            }
+        });
+
+        //  设置消息抵达队列回调
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            /**
+             * 只要消息没有投递给指定的队列，就会触发这个失败回调
+             *
+             * @param message    消息
+             * @param replyCode  回复代码
+             * @param replyText  回复文本
+             * @param exchange   交换
+             * @param routingKey 路由的关键
+             */
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("Fail Message{"+message+"},replyCode:"+replyCode+",replyText{"+replyText+"},exchange:["+exchange+"],routingKey:["+routingKey+"]");
             }
         });
     }
