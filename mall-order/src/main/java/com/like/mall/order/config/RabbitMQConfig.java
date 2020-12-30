5f8692a32c2681a7a567a1679647a17b90a60280
like <980650920@qq.com>
2020-12-30 15:41:17 +0800

    RabbitMQ设置publisher-confirms
    Signed-off-by: like <980650920@qq.com>

--- mall-order/src/main/java/com/like/mall/order/config/RabbitMQConfig.java
+++ mall-order/src/main/java/com/like/mall/order/config/RabbitMQConfig.java
@@ -1,9 +1,14 @@
 package com.like.mall.order.config;
 
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 
 /**
  * @author like
@@ -14,8 +19,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @PostConstruct
+    public void initRabbitTemplate() {
+        // 设置确认回调-确认抵达
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean b, String s) {
+                System.out.println("confirm{correlationData:"+correlationData+",ack:"+b+",cause:"+s);
+            }
+        });
     }
 }
