ee7ba64e3beec8883217b62f148fea3f6cbf944a
like <980650920@qq.com>
2021-01-27 17:08:51 +0800

    添加秒殺服務
    Signed-off-by: like <980650920@qq.com>

--- mall-coupon/src/main/java/com/like/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ mall-coupon/src/main/java/com/like/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,16 +1,17 @@
 package com.like.mall.coupon.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.like.mall.common.utils.PageUtils;
 import com.like.mall.common.utils.Query;
-
 import com.like.mall.coupon.dao.SeckillSkuRelationDao;
 import com.like.mall.coupon.entity.SeckillSkuRelationEntity;
 import com.like.mall.coupon.service.SeckillSkuRelationService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +19,14 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<SeckillSkuRelationEntity> query = new QueryWrapper<>();
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        if (StringUtils.isNotBlank(promotionSessionId)) {
+            query.eq("promotion_session_id", promotionSessionId);
+        }
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                query
         );
 
         return new PageUtils(page);
